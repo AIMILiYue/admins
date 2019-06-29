2068ba1c04aaaf530786333da75fab03dc67ae81
ityuer <ityuer@ityuer.com>
2019-06-29 15:21:22 +0800

    调整src目录结构

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 默认去找router.js文件，没有文件去找router文件夹，找到文件夹，默认去找index.sjs文件
+// 划分路由文件的目的：可以对模块进行路由分类，方便管理维护
 import router from './router'
 
 Vue.config.productionTip = false
